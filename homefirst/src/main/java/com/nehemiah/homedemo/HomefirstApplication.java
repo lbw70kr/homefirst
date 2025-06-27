e9c51ea482759b8c17106930b288e35437155830
lbw70kr <lbw70kr@gmail.com>
2025-06-27 16:48:47 +0900

    수정 테스트

--- homefirst/src/main/java/com/nehemiah/homedemo/HomefirstApplication.java
+++ homefirst/src/main/java/com/nehemiah/homedemo/HomefirstApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomefirstApplication {
 
 	public static void main(String[] args) {
-		
+		//테스트
 		SpringApplication.run(HomefirstApplication.class, args);
 	}
 
