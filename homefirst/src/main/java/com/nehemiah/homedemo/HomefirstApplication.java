a454061cac9e6641660e2707256a142946837566
lbw70kr <122728919+lbw70kr@users.noreply.github.com>
2025-06-27 15:33:34 +0900

    최초수정

--- homefirst/src/main/java/com/nehemiah/homedemo/HomefirstApplication.java
+++ homefirst/src/main/java/com/nehemiah/homedemo/HomefirstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomefirstApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HomefirstApplication.class, args);
 	}
 
